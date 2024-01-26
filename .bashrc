13331fa3179ca463e4a2d3777df031125f461353
angelf015 <angelf015.lf@gmail.com>
2024-01-26 11:37:36 -0600

    Update configuration

--- .bashrc
+++ .bashrc
@@ -1,18 +1,8 @@
 #
-# ~/.bashrc
-#
-
-# If not running interactively, don't do anything
-# [[ $- != *i* ]] && return
 
 alias ls='ls --color=auto'
 alias grep='grep --color=auto'
-# PS1='[\u@\h \W]\$ '
-#
-#set -gx PATH $HOME/.local/bin $PATH
 
-#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
-[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
 eval "$(starship init bash)"
 # Use bash-completion, if available
 [[ $PS1 && -f /usr/share/bash-completion/bash_completion ]] && \
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -20,3 +20,24 @@ starship init fish | source
 
 #set -gx PATH $HOME/.sdkman/bin/sdkman-init.sh
 #set -gx PATH $PATH ~/.sdkman/candidates/java/18.0.1-open/bin/
+set -gx PATH $HOME/.config/vifm/ $PATH
+#alias vifm vifmrun
+# Input prompt of fuzzy searcher
+set -g FONTPREVIEW_SEARCH_PROMPT "❯ "
+
+# Size of the font preview window
+set -g FONTPREVIEW_SIZE 532x365
+
+# The position where the font preview window should be displayed
+set -g FONTPREVIEW_POSITION "+0+0"
+
+# Font size
+set -g FONTPREVIEW_FONT_SIZE 38
+
+# Background color of the font preview window
+set -g FONTPREVIEW_BG_COLOR "#1e1e2e"
+
+# Foreground color of the font preview window
+set -g FONTPREVIEW_FG_COLOR "#f38ba8"
+set -gx PATH $HOME/.config/emacs/bin $PATH
+
